e9cd5068c454cd2416126fb2ca375bf13e7f2172
harishvinay <harishvinay@gmail.com>
2017-09-01 14:39:33 +0530

    initial instalment of poc

---
+++ js/main.js
@@ -0,0 +1,55 @@
+'use strict';
+
+requirejs.config(
+    {
+        baseUrl: 'js',
+
+        paths:
+            {
+                'knockout': '../node_modules/knockout/build/output/knockout-latest.debug',
+                'jquery': '../node_modules/jquery/dist/jquery',
+                'jqueryui-amd': '../node_modules/jquery-ui/ui',
+                'promise': '../node_modules/es6-promise/dist/es6-promise',
+                'hammerjs': '../node_modules/hammerjs/hammer',
+                'ojdnd': '../node_modules/oraclejet/dist/js/libs/dnd-polyfill/dnd-polyfill-1.0.0',
+                'ojs': '../node_modules/oraclejet/dist/js/libs/oj/debug',
+                'ojL10n': '../node_modules/oraclejet/dist/js/libs/oj/ojL10n',
+                'ojtranslations': '../node_modules/oraclejet/dist/js/libs/oj/resources',
+                'text': '../node_modules/requirejs-text/text',
+                'signals': '../node_modules/signals/dist/signals',
+                'customElements': '../node_modules/oraclejet/dist/js/libs/webcomponents/CustomElements',
+                'metadata-list-component': 'libs/metadata-list-component/metadata-list-component-noDeps'
+            },
+        shim:
+            {
+                'jquery':
+                    {
+                        exports: ['jQuery', '$']
+                    }
+            }
+    }
+);
+
+require(['ojs/ojcore', 'knockout', 'appController', 'ojs/ojknockout',
+        'ojs/ojmodule', 'ojs/ojrouter', 'ojs/ojnavigationlist', 'ojs/ojbutton', 'ojs/ojtoolbar'],
+    function (oj, ko, app) {
+        $(function () {
+            function init() {
+                oj.Router.sync().then(
+                    function () {
+                        ko.applyBindings(app, document.getElementById('globalBody'));
+                    },
+                    function (error) {
+                        oj.Logger.error('Error in root start: ' + error.message);
+                    }
+                );
+            }
+
+            if ($(document.body).hasClass('oj-hybrid')) {
+                document.addEventListener("deviceready", init);
+            } else {
+                init();
+            }
+        });
+    }
+);
